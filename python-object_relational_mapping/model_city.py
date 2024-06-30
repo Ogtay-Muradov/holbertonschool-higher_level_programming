daaffd8e706758c50b87eb9729eac7f6dc2153d8
Ogtay-Muradov <110946781+Ogtay-Muradov@users.noreply.github.com>
2024-06-30 16:13:04 +0400

    Update model_city.py

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """
-Contains the class definition of a City
+This script connects to a MySQL database
 """
+
 from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class City(Base):
-    """
-    Class  that defines each city
-    """
-    __tablename__ = "cities"
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    """A class that represents a state"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False,
+                autoincrement=True, unique=True)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
